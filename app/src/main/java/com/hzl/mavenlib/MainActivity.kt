13a441cc9a7a65a3d8da7617a59531fbb9fa6df5
15697901753@163.com <661161900>
2018-07-16 10:50:02 +0800

    two

---
+++ app/src/main/java/com/hzl/mavenlib/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hzl.mavenlib
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
